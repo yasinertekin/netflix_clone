dd6b358dbc8019c7f549af7554292bba2793f22e
yasinertekin <yasinertekinn@outlook.com>
2023-09-04 22:26:04 +0300

    avatar selection screen edited

--- lib/feature/Profile/Avatar Select/avatar_select.dart
+++ lib/feature/Profile/Avatar Select/avatar_select.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
-import 'package:netflix_clone/feature/Profile/Add%20Profile/add_profile.dart';
+import 'package:netflix_clone/feature/Profile/Profile%20List/create_select_profile_screen.dart';
 import 'package:netflix_clone/feature/Profile/Profile%20List/create_select_profile_view_model.dart';
+import 'package:netflix_clone/product/widgets/Card/avatar_card.dart';
+import 'package:netflix_clone/product/widgets/Card/selected_photo_card_image.dart';
+import 'package:netflix_clone/product/widgets/Icon%20Button/edit_icon_button.dart';
 
 class SelectAvatarCardS extends StatelessWidget {
   const SelectAvatarCardS({super.key, required this.viewModel});
@@ -78,54 +83,110 @@ class _AvatarList extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            InkWell(
-              onTap: () {},
-              child: const CircleAvatar(
-                backgroundColor: Colors.transparent,
-                child: Icon(Icons.arrow_back),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 125.0,
-              ),
-              child: Text(
-                'Choose Icon',
-                style: context.general.textTheme.bodyLarge!.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
-        SizedBox(
-          height: context.dynamicHeight(0.5),
-          child: ListView.builder(
-            itemCount: snapshot.data!.length,
-            scrollDirection: Axis.vertical,
-            itemBuilder: (context, index) {
-              final characterData = snapshot.data![index];
-              final name = characterData['name'] as String;
-              final photos = characterData['photos'] as List<dynamic>;
+        const _AvatarSelectHeader(),
+        AvatarAndNameList(snapshot: snapshot, viewModel: viewModel),
+      ],
+    );
+  }
+}
 
-              return ListTile(
-                title: Text(
-                  name,
-                  style: context.general.textTheme.bodyLarge!.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: SizedBox(
-                  height: context.dynamicHeight(0.20),
-                  child: _AvatarHorizantalList(photos: photos, viewModel: viewModel),
-                ),
-              );
-            },
+class AvatarAndNameList extends StatelessWidget {
+  const AvatarAndNameList({
+    super.key,
+    required this.snapshot,
+    required this.viewModel,
+  });
+
+  final AsyncSnapshot<List<Map<String, dynamic>>> snapshot;
+  final CreateSelectProfileViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.dynamicHeight(0.5),
+      child: ListView.builder(
+        itemCount: snapshot.data!.length,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          final characterData = snapshot.data![index];
+          final name = characterData['name'] as String;
+          final photos = characterData['photos'] as List<dynamic>;
+
+          return AvatarAndNameListTile(name: name, photos: photos, viewModel: viewModel);
+        },
+      ),
+    );
+  }
+}
+
+class AvatarAndNameListTile extends StatelessWidget {
+  const AvatarAndNameListTile({
+    super.key,
+    required this.name,
+    required this.photos,
+    required this.viewModel,
+  });
+
+  final String name;
+  final List photos;
+  final CreateSelectProfileViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: _CharactarName(name: name),
+      subtitle: SizedBox(
+        height: context.dynamicHeight(0.20),
+        child: _AvatarHorizantalList(photos: photos, viewModel: viewModel),
+      ),
+    );
+  }
+}
+
+class _CharactarName extends StatelessWidget {
+  const _CharactarName({
+    required this.name,
+  });
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      name,
+      style: context.general.textTheme.bodyLarge!.copyWith(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class _AvatarSelectHeader extends StatelessWidget {
+  const _AvatarSelectHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        InkWell(
+          onTap: () {},
+          child: const CircleAvatar(
+            backgroundColor: Colors.transparent,
+            child: Icon(Icons.arrow_back),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 125.0,
+          ),
+          child: Text(
+            'Choose Icon',
+            style: context.general.textTheme.bodyLarge!.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ],
@@ -155,22 +216,7 @@ class _AvatarHorizantalList extends StatelessWidget {
               viewModel.newPhotoURL = photos[photoIndex];
               Navigator.pop(context);
             },
-            child: Card(
-              color: context.randomColor,
-              elevation: 0,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  photos[photoIndex],
-                  fit: BoxFit.cover,
-                  height: context.dynamicHeight(0.25),
-                  width: context.dynamicWidth(0.32),
-                ),
-              ),
-            ),
+            child: AvatarCard(photos: photos, photoIndex: photoIndex),
           ),
         );
       },
