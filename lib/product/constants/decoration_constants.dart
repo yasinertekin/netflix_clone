51fe8962f15e6bb9ddc26c69aeb2284daf4da667
yasinertekin <yasinertekinn@outlook.com>
2023-09-07 23:42:32 +0300

    Added default border radius and low Border Radius values.

---
+++ lib/product/constants/decoration_constants.dart
@@ -0,0 +1,5 @@
+class DecorationConstants {
+  const DecorationConstants._();
+  static const double defaultBorderRadius = 20.0;
+  static const double lowBorderRadius = 10.0;
+}
--- lib/product/constants/index.dart
+++ lib/product/constants/index.dart
@@ -1,2 +1,3 @@
 export 'color_constants.dart';
+export 'int_constants.dart';
 export 'string_constants.dart';
