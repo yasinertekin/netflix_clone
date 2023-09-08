d1c410e57a9f8f22f3756355f1f7abb2a9982a0b
yasinertekin <yasinertekinn@outlook.com>
2023-09-08 23:35:45 +0300

    Added avatar card to tab bar profile section

--- lib/feature/TabBar/tab_bar_screen.dart
+++ lib/feature/TabBar/tab_bar_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 import 'package:netflix_clone/feature/Home/home_view.dart';
-import 'package:netflix_clone/feature/Profile/profile_view.dart';
+import 'package:netflix_clone/feature/Profile/Profile%20View/profile_view.dart';
+import 'package:netflix_clone/product/constants/color_constants.dart';
+import 'package:netflix_clone/product/constants/string_constants.dart';
+import 'package:netflix_clone/product/enums/tab_bar_enums.dart';
+import 'package:netflix_clone/product/widgets/Card/avatar_card.dart';
 
 // ignore: must_be_immutable
 class TabBarScreen extends StatefulWidget {
@@ -20,78 +24,69 @@ class _TabBarScreenState extends State<TabBarScreen> with TickerProviderStateMix
   @override
   void initState() {
     super.initState();
-    _tabController =
-        TabController(length: _MyTabViews.values.length, vsync: this, initialIndex: widget.initialTabIndex);
+    _tabController = TabController(length: MyTabviews.values.length, vsync: this, initialIndex: widget.initialTabIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    const notchedValue = 10.0;
     return DefaultTabController(
-      length: _MyTabViews.values.length,
+      length: MyTabviews.values.length,
       child: Scaffold(
         extendBody: true,
         bottomNavigationBar: BottomAppBar(
-          height: context.dynamicHeight(0.1),
           color: Colors.black,
-          notchMargin: notchedValue,
-          child: _MyTabView(),
+          child: myTabView(),
         ),
-        body: _tabBarView(tabController: _tabController, profileName: widget.profileName),
+        body: _TabBarView(tabController: _tabController, profileName: widget.profileName),
       ),
     );
   }
 
-  _MyTabView() {
-    return SizedBox(
-        child: TabBar(
-      indicatorColor: Colors.transparent,
-      dividerColor: Colors.transparent,
-
-      // isScrollable: true,
-      labelColor: Colors.white,
-      unselectedLabelColor: Colors.grey,
+  myTabView() {
+    return TabBar(
+      indicatorSize: TabBarIndicatorSize.label,
+      indicatorColor: ColorConstants.black,
+      dividerColor: ColorConstants.transparant,
+      indicatorWeight: 3,
+      tabAlignment: TabAlignment.fill,
+      //isScrollable: true,
+      labelColor: ColorConstants.white,
+      unselectedLabelColor: ColorConstants.grey,
       padding: EdgeInsets.zero,
       onTap: (value) {
         //int value
       },
       controller: _tabController,
-      tabs: _MyTabViews.values.map((e) {
-        if (e.name == 'Home') {
+      tabs: MyTabviews.values.map((e) {
+        if (e.name == 'home') {
           return Tab(
             text: e.name,
-            icon: const Icon(Icons.home, size: 22),
+            icon: const Icon(Icons.home, size: 20),
           );
-        } else if (e.name == 'New') {
+        } else if (e.name == 'news') {
           return Tab(
             text: e.name,
-            icon: const Icon(Icons.new_label_sharp, size: 22),
+            icon: const Icon(Icons.new_label_sharp, size: 20),
           );
         } else {
-          // Diğer durumlar için bir resim göstermek isterseniz, Image.asset veya Image.network kullanabilirsiniz.
-          // Aşağıda örnek bir Image.network kullanımı verilmiştir:
           return Tab(
             text: e.name,
-            icon: SizedBox(
-              width: 45,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Image.network(
-                  height: 45,
-                  fit: BoxFit.cover,
-                  widget.profileImage!,
-                ),
+            icon: CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.transparent,
+              child: AvatarCard(
+                photoURL: widget.profileImage ?? StringConstans.randomImageURL,
               ),
             ),
           );
         }
       }).toList(),
-    ));
+    );
   }
 }
 
-class _tabBarView extends StatelessWidget {
-  const _tabBarView({
+class _TabBarView extends StatelessWidget {
+  const _TabBarView({
     required TabController tabController,
     required this.profileName,
   }) : _tabController = tabController;
@@ -101,18 +96,18 @@ class _tabBarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TabBarView(physics: const NeverScrollableScrollPhysics(), controller: _tabController, children: [
-      HomeView(
-        profileName: profileName,
-      ),
-      Container(
-        color: Colors.red,
-      ),
-      const ProfileView(),
-    ]);
+    return TabBarView(
+      physics: const NeverScrollableScrollPhysics(),
+      controller: _tabController,
+      children: [
+        HomeView(
+          profileName: profileName,
+        ),
+        Container(
+          color: Colors.red,
+        ),
+        const ProfileView(),
+      ],
+    );
   }
 }
-
-enum _MyTabViews { Home, New, Profile }
-
-extension _MyTabViewExtension on _MyTabViews {}
